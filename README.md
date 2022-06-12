c14e79d2882cef3f0befa98502d7a551ea338627
pablu41 <100160123+pablu41@users.noreply.github.com>
2022-06-12 20:28:23 +0600

    ok

---
+++ README.md
@@ -0,0 +1,2 @@
+# assignment_17
+php language
---
+++ includes/navigation.php
@@ -0,0 +1,39 @@
+<!-- Main navigation -->
+<div class="sidebar-category sidebar-category-visible">
+						<div class="category-content no-padding">
+							<ul class="navigation navigation-main navigation-accordion">
+
+								<!-- Main -->
+								<li class="active"><a href="#"><i class="icon-home4"></i> <span>Dashboard</span></a></li>
+								<li class="#"><a href="banner.php"><i class="icon-image-compare"></i> <span>Banners</span></a></li>
+								<li class="#"><a href="#"><i class="icon-home4"></i> <span>Services</span></a></li>
+								<li class="#"><a href="#"><i class="icon-home4"></i> <span>Sections</span></a></li>
+								<li class="#"><a href="#"><i class="icon-home4"></i> <span>Oru Projects</span></a></li>
+								<li class="#"><a href="#"><i class="icon-home4"></i> <span>Oru Staff</span></a></li>
+								<li class="#"><a href="#"><i class="icon-home4"></i> <span>Oru Client</span></a></li>
+								<li class="#"><a href="#"><i class="icon-home4"></i> <span>Contact Message</span></a></li>
+								<li>
+									<a href="#"><i class="icon-gear"></i> <span>Back Office Setup</span></a>
+									<ul>
+										<li><a href="#">Category</a></li>
+										<li><a href="#">Designation</a></li>							
+									</ul>
+								</li>																
+								<li>
+									<a href="#"><i class="icon-stack"></i> <span>Starter kit</span></a>
+									<ul>
+										
+										<li>
+											<a href="#">3 columns</a>
+											<ul>
+												<li><a href="starters/3_col_dual.html">Dual sidebars</a></li>												
+											</ul>
+										</li>										
+									</ul>
+								</li>								
+								<!-- /main --> 								
+
+							</ul>
+						</div>
+					</div>
+					<!-- /main navigation -->
